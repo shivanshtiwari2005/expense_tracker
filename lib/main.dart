4c49f44fd9a06c5e8489caed1de3da6c14a24e94
Shivansh Tiwari <sachintiwari.edu@gmail.com>
2025-02-26 23:48:02 +0530

    Implements expense tracker app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+
+import 'package:expanse_tracker/expenses.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+
+    ),
+  );
+}
